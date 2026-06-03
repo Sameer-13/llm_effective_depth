ac0fac8abf9f186b694980dc87806d266c440cad
Sameer Alsabea <sameeralsabea@Sameers-MacBook-Air.local>
2026-06-03 13:54:39 +0300

    add gemma4

--- analysis/lib/models.py
+++ analysis/lib/models.py
@@ -11,13 +11,14 @@ local_model_table = {
     "qwen2.5_14b": ("Qwen/Qwen2.5-14B", True),
     "qwen2.5_7b": ("Qwen/Qwen2.5-7B", True),
     "qwen2.5_1.5b": ("Qwen/Qwen2.5-1.5B", True),
-    "qwen3_8b": ("Qwen/Qwen3-8B", True),
+    "qwen3_8b": ("Qwen/Qwen3-8B", False),
     "qwen3_14b": ("Qwen/Qwen3-14B", True),
     "qwen3_32b": ("Qwen/Qwen3-32B", True),
     "llama_3.1_8b": ("meta-llama/Meta-Llama-3.1-8B", True),
     "llama_3.1_405b": ("meta-llama/Meta-Llama-3.1-405B", True),
     "llama_3.1_8b_instruct": ("meta-llama/Meta-Llama-3.1-8B-Instruct", True),
     "llama_3.1_70b_instruct": ("meta-llama/Meta-Llama-3.1-70B-Instruct", True),
+    "gemma_4_e4b": ("google/gemma-4-E4B-it", False),
 }
 
 def get_model(model_name):
